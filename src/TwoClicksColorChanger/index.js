b5bdcdc46e42ce02617f9c6f4eb020a32fa2e906
Hendro Lee <hendrolee39@gmail.com>
2022-05-07 18:11:51 +1000

    Initialize project directory

---
+++ src/TwoClicksColorChanger/index.js
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+const TwoClicksColorChanger = () => {
+  return (
+    <div>
+      <h1>hello world</h1>
+    </div>
+  );
+};
+
+export default TwoClicksColorChanger;
