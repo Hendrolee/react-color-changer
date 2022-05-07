96cfa52e5ef6c82e5c3862d463d0de7499f4f41c
Hendro Lee <hendrolee39@gmail.com>
2022-05-07 18:37:24 +1000

    Create RGB component

--- src/TwoClicksColorChanger/index.js
+++ src/TwoClicksColorChanger/index.js
@@ -1,9 +1,16 @@
 import { useState } from "react";
+import "./index.css";
 
 const TwoClicksColorChanger = () => {
+  const [color, setColor] = useState("white");
+
   return (
-    <div>
-      <h1>hello world</h1>
+    <div className="Container" style={{ backgroundColor: color }}>
+      <h1>Change colors</h1>
+
+      <button onClick={() => setColor("red")}>Red</button>
+      <button onClick={() => setColor("green")}>Green</button>
+      <button onClick={() => setColor("blue")}>Blue</button>
     </div>
   );
 };
